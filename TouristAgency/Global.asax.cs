3adb39596e7fed71077223519e0e009e33aead08
Mirza Vucijak <mvucijak1@etf.unsa.ba>
2019-05-13 22:00:27 +0200

    Register webapi

---
+++ TouristAgency/Global.asax.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace TouristAgency
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            GlobalConfiguration.Configuration.EnsureInitialized();
+        }
+    }
+}
